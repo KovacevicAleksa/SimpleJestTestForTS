315c892b6058cac9f9852f49e74cc14168284bc1
KovacevicAleksa <aleksakovacevicc@gmail.com>
2024-06-07 22:26:26 +0200

    added parameterized test

---
+++ dist/main.ts
@@ -0,0 +1,9 @@
+export function getNumber(a: number, b: number): number {
+  return a + b;
+}
+export function getString(a: string, b: string): string {
+  return a + b;
+}
+export function legalAge(a: number): boolean {
+  return a >= 18 ? true : false;
+}
