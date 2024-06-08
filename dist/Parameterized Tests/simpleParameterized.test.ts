b70ac87e81c2da345deec494339851007ce7710e
KovacevicAleksa <aleksakovacevicc@gmail.com>
2024-06-08 21:07:56 +0200

    added parameterized test

--- dist/Parameterized Tests/simpleParameterized.test.ts
+++ dist/Parameterized Tests/simpleParameterized.test.ts
@@ -3,3 +3,7 @@ import { legalAge } from "../main.ts";
 it.each([18, 99])("should return true when age is %i", (nb) => {
   expect(legalAge(nb)).toBe(true);
 });
+
+it.each([17, 1])("should return false when age is %i", (nb) => {
+  expect(legalAge(nb)).toBe(false);
+});
